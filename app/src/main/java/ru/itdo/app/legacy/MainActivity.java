cd4b390c48343aa0c486cb2b3cfb38fccedff308
minibleyz <a32526132@gmail.com>
2026-08-28 16:01:30 +0300

    Migrate to AndroidX + Material Components (M2)
    - gradle.properties: enable useAndroidX/enableJetifier
    - app/build.gradle: replace com.android.support:* with androidx.* and
      com.google.android.material:material (M2)
    - styles.xml: Theme.AppCompat -> Theme.MaterialComponents
    - activity_main.xml: support-design widgets -> androidx/material widgets
    - MainActivity.java: android.support.* imports -> androidx.* / com.google.android.material.*
    
    Replaces the custom AppCompat-only theme with standard Material
    Components (M2) styling (toolbar, FAB, snackbar) per Material.io.

--- app/src/main/java/ru/itdo/app/legacy/MainActivity.java
+++ app/src/main/java/ru/itdo/app/legacy/MainActivity.java
@@ -4,12 +4,12 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
